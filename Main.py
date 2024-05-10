8d98c19a356083a74451d9524ce3507c59aed9fe
Mystic <manognachamarthi@gmail.com>
2024-05-10 17:15:09 +0530

    Screen Drawn

--- Main.py
+++ Main.py
@@ -1,3 +1,23 @@
 import pygame
 
-print("Hello")
+#pygame Setup
+pygame.init()
+screen = pygame.display.set_mode((1200,720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    #filling Screen
+    screen.fill("Blue")
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
+    
